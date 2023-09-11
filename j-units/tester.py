0bb58bc720c7a5a23add1efb5d912e85595440fe
Kinga <kinga@gwu.edu>
2023-09-11 10:13:59 -0400

    J1

--- j-units/tester.py
+++ j-units/tester.py
@@ -5,11 +5,11 @@ def good(input, output):
 	searchObj = re.search( r'^checkCode [1-9]\d?\d?\.([A-Z][A-Za-z]* )+ *(\d\d\d\d\d)?$', input, re.M)
 	if searchObj:
 		if output != "true":
-			print "Error, on input '" + input + "', we got 'true' but you expected '" + output + "'"
+			print ("Error, on input '" + input + "', we got 'true' but you expected '" + output + "'")
 			return False
 	else:
 		if output != "false":
-			print "Error, on input '" + input + "', we got 'false' but you expected '" + output + "'"
+			print ("Error, on input '" + input + "', we got 'false' but you expected '" + output + "'")
 			return False	
 	return True	
 	
@@ -50,19 +50,19 @@ ctr = 1
 for input in inputs:
 	result = good(input, outputs[input])
 	if not result:
-		print "for test case " + str(ctr)
+		print ("for test case " + str(ctr))
 	correct = correct and result
 	ctr += 1
 	
 if correct:
-	print "All of your test cases were correct!"
+	print ("All of your test cases were correct!")
 	
 bug = False
 for input in inputs:
 	if bad(input, outputs[input]) and correct:
 		bug = True
-		print "Great job! You found the bug"
+		print ("Great job! You found the bug")
 		
 if not bug and correct:
-	print "Try writing more test cases. You did not find the bug."
+	print ("Try writing more test cases. You did not find the bug.")
 	
